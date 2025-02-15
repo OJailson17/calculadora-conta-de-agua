5c52f081023fd71b65750ff197f6efeb0e05f3b8
OJailson17 <JaylsonO17@gmail.com>
2025-02-15 19:54:00 -0300

    chore: update personal website link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const LINKS = {
 	billImage:
 		'https://www.embasa.ba.gov.br/documents/2077661/2080681/mcp072013.png/f69311da-88dc-8560-7375-dc62b38d9a08?t=1676350627482',
 	taxes: 'https://www.embasa.ba.gov.br/w/tarifas-2024',
-	personalWebsite: 'https://jailsondeoliveira.vercel.app',
+	personalWebsite: 'https://jailsondeoliveira.com.br',
 	personalGithub: 'https://github.com/OJailson17',
 	personalLinkedIn: 'https://www.linkedin.com/in/ojailson17',
 	personalInstagram: 'https://www.instagram.com/ojailson17',
