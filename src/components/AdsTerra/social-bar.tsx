1800963b33dfe7dd0d2d6532eca79ea3bcb910c2
OJailson17 <JaylsonO17@gmail.com>
2025-12-02 03:40:39 -0300

    chore: add social bar ad

---
+++ src/components/AdsTerra/social-bar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Script from "next/script";
+
+export const SocialBar = () => {
+  return (
+    <Script
+      src="//pl28173358.effectivegatecpm.com/8e/1b/20/8e1b2014e5a507a70d652b320f71b53b.js"
+      strategy="afterInteractive"
+      async
+    />
+  );
+};
